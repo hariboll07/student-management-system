0a3cf2d19e50f0eba5028134ca8027ab46000a34
hariboll07 <kiranbolla07@gmail.com>
2026-03-13 13:24:01 +0530

    Updated file

--- frontend/src/context/APIcontext.jsx
+++ frontend/src/context/APIcontext.jsx
@@ -4,7 +4,7 @@ export const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
 
-  const API_URL = "https://student-management-system-fras.onrender.com";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   return (
     <ApiContext.Provider value={{ API_URL }}>
